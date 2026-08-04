6ee247a643c938d141ab62393da549b666198658
Chris Chaples <33135543+chaples55@users.noreply.github.com>
2026-08-03 20:24:22 -0400

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.acousticfish.wheelielauncher"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
